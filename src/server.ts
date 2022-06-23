1dab30eedd43fa26f536f51117cd21ca3abe7eef
Mateus <mateushbsb@gmail.com>
2022-06-23 15:27:12 -0300

    feat[config-ts-node-dev]: add ts-node-dev for auto reload

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,18 @@
-console.log("Hellow word");
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (request, response) => {
+  return response.json({ message: "ok" });
+});
+
+app.post("/courses", (request: Request, response: Response) => {
+  const { name } = request.body;
+  console.log(name);
+
+  return response.json({ name });
+});
+
+app.listen(3333, () => console.log("server is running!"));
