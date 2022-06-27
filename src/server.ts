865971ead2006c79aa9aa93605716fb8608d18b1
Mateus <mateushbsb@gmail.com>
2022-06-27 09:29:32 -0300

    insert type of categories

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,11 @@
-import express, { Request, Response } from "express";
+import express from "express";
+
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (request, response) => {
-  return response.json({ message: "ok" });
-});
-
-app.post("/courses", (request: Request, response: Response) => {
-  const { name } = request.body;
-  console.log(name);
-
-  return response.json({ name });
-});
+app.use("/categories", categoriesRoutes);
 
 app.listen(3333, () => console.log("server is running!"));
